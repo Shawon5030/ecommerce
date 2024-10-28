82192f2e0096c2decade60c858d1962a5adddc16
unknown <haquemahmudul600@gmail.com>
2024-10-29 01:45:24 +0600

    Auto commit

--- Shop/views.py
+++ Shop/views.py
@@ -2,4 +2,6 @@ import product
 # Create your views here.
 def home(request):
           all_Product = product.objects.all()
-               return render(request, 'Shop/home.html' ,{'a'})
+               return render(request, 'Shop/home.html' ,{'all_Product': all_Product})
+
+               def product_detail(req
