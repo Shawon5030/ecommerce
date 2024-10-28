9cccb9f3b9e2ce863e2f05cb7f03bacc13057bdd
unknown <haquemahmudul600@gmail.com>
2024-10-29 01:45:29 +0600

    Auto commit

--- Shop/views.py
+++ Shop/views.py
@@ -4,4 +4,5 @@ def home(request):
           all_Product = product.objects.all()
                return render(request, 'Shop/home.html' ,{'all_Product': all_Product})
 
-               def product_detail(req
+               def product_detail(request):
+                     return render(request, 'Shop/productdetail
