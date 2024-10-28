48c4bf889d95b7a428f9eb94c550a368dd302f14
unknown <haquemahmudul600@gmail.com>
2024-10-29 01:01:01 +0600

    b

---
+++ javascript.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+setInterval(() => {
+  exec("git add . && git commit -m 'Auto commit'", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error}`);
+      return;
+    }
+    console.log(`Output: ${stdout}`);
+  });
+}, 5000); // 5000ms = 5 seconds
+
+exec("git add . && git commit -m 'Auto commit' && git push origin main", (error, stdout, stderr) => {
