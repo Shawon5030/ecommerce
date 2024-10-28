4a357fff6cadef90250990e46225af838bd4d936
unknown <haquemahmudul600@gmail.com>
2024-10-29 01:06:20 +0600

    Auto commit

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 setInterval(() => {
-  exec("git add . && git commit -m 'Auto commit'", (error, stdout, stderr) => {
+  exec(`git add . && git commit -m "Auto commit" && git push origin main`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error}`);
       return;
@@ -9,5 +9,3 @@ setInterval(() => {
     console.log(`Output: ${stdout}`);
   });
 }, 5000); // 5000ms = 5 seconds
-
-exec("git add . && git commit -m 'Auto commit' && git push origin main", (error, stdout, stderr) => {
